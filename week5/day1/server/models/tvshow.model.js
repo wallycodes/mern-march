71ec424b5de40fda18d32f193951701468354e69
Waldimir Perez <wallycodes@gmail.com>
2023-04-09 19:56:11 -0600

    Wrote Validations

--- week5/day1/server/models/tvshow.model.js
+++ week5/day1/server/models/tvshow.model.js
@@ -4,19 +4,32 @@ const mongoose = require('mongoose');
  
 const TvShow = new mongoose.Schema({
     title: {
-        type: String
+        type: String,
+        required:[true, 'Title is required'],
+        minLength:[3,'The title must be 3 or more characters'],
+        maxLength:[10, 'The title is too long']
     },
     releaseYear: {
-        type: Number
+        type: Number,
+        min:[1920, 'No sows before 1920 allowed']
     },
     network: {
-        type: String
+        type: String,
+        required:[true, 'Network is required'],
+        minLength:[3,'The network must be 3 or more characters'],
+        maxLength:[10, 'The network is too long']
     },
     creator: {
-        type: String
+        type: String,
+        required:[true, 'Creator is required'],
+        minLength:[3,'The creator must be 3 or more characters'],
+        maxLength:[10, 'The creator is too long']
     },
     genre: {
-        type: String
+        type: String,
+        required:[true, 'Genre is required'],
+        minLength:[3,'The genre must be 3 or more characters'],
+        maxLength:[10, 'The genre is too long']
     }
 });
  
