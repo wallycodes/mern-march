8e8a19e42acd45ecad6af41a2c31839303cfb502
Waldimir Perez <wallycodes@gmail.com>
2023-04-13 16:02:16 -0600

    added axios

--- week5/projectm1/client/src/App.js
+++ week5/projectm1/client/src/App.js
@@ -1,9 +1,20 @@
 import Form from './components/Form'
 import './App.css';
-import { useState } from 'react';
+import { useState, useEffect} from 'react';
+import axios from 'axios';
 
 
 function App(props) {
+
+useEffect(()=> {
+  axios.get('http://localhost:8000/api/allProjects')
+  .then((response) => {
+    setProjectList(response.data.projects)
+  })
+  .catch((err) => {
+    console.log(err);
+  })
+},[])
 
   const [projectList, setProjectList] = useState([])
 
